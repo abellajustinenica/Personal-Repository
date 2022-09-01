7ec10003a349ca40e75bfc7445e8fab064eaf9b9
Abella, Justine Nica <111735968+abellajustinenica@users.noreply.github.com>
2022-09-01 16:50:06 +0800

    Create Playlist.java

---
+++ Manual_Implementation/Doubly/Playlist.java
@@ -0,0 +1,11 @@
+public class Playlist {
+    // attributes/properties
+    String title;
+    Playlist next;
+    Playlist previous;
+
+    Playlist(String songTitle) {
+        title = songTitle;
+    }
+
+}
